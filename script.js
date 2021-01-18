b477ec4976ab49e53c82fe03ef6921490054147e
Andrew Braun <andrew@andrewbraun.dev>
2021-01-18 10:29:06 +0400

    refactor: Clean up event handlers; style: H1 text effects

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const infoDisplay = document.querySelector("#display");
 const drumpadButtonsArr = Array.from(drumpadButtons);
 const drumpadKeysArr = Array.from(drumpadKeys);
 
-if (window.location.href.includes("codepen")) {
-	console.log("codepen!");
-}
-
+// Stop target audio element and start it again.
 function playAudio(clip) {
 	if (clip) {
 		clip.currentTime = 0;
@@ -17,6 +14,7 @@ function playAudio(clip) {
 	}
 }
 
+// Use audio element ID to create display description
 function renderDescription(id) {
 	infoDisplay.innerText = id
 		.split("-")
@@ -24,16 +22,22 @@ function renderDescription(id) {
 		.join(" ");
 }
 
+// Add animation to drumheads on click or keypress
 function animateDrumhead(target) {
 	target.classList.add("active-drumhead");
 	target.onanimationend = () => target.classList.remove("active-drumhead");
 }
 
-function handleDrumpadEvent(event) {
-	const audio = event.target.querySelector("audio");
+// Using audio container div, play audio and render changes to DOM
+function activateDrumhead(target) {
+	const audio = target.querySelector("audio");
 	playAudio(audio);
-	renderDescription(event.target.id);
-	animateDrumhead(event.target);
+	renderDescription(target.id);
+	animateDrumhead(target);
+}
+
+function handleDrumpadEvent(event) {
+	activateDrumhead(event.target);
 }
 
 function handleKeyPress(event) {
@@ -41,16 +45,12 @@ function handleKeyPress(event) {
 		(item) => item.id === event.key.toUpperCase()
 	);
 	if (clip) {
-		playAudio(clip);
-		renderDescription(clip.parentElement.id);
-		animateDrumhead(clip.parentElement);
+		activateDrumhead(clip.parentElement);
 	}
 }
 // Add event listener to each drumpad button
 for (element of drumpadButtonsArr) {
 	element.addEventListener("click", handleDrumpadEvent);
 }
-
-console.log(drumpadButtonsArr);
 
 window.addEventListener("keydown", handleKeyPress);
